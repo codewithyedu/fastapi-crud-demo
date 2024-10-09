3432796cbb310446d3071d8bc9dc073f8fc5ca51
Yedukrishna <codewithyedu@gmail.com>
2024-10-09 20:03:37 +0530

    Add get_user_by_name method to crud.py

--- src/crud.py
+++ src/crud.py
@@ -8,6 +8,10 @@ def get_user(db: Session, user_id: str):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
 
+def get_user_by_name(db: Session, user_name: str):
+    return db.query(models.User).filter(models.User.name == user_name).first()
+
+
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
